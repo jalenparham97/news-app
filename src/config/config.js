dc030c55091e36737028e5f0ba3bc96442a1fe3b
jalen <jalenparham97@gmail.com>
2019-02-02 13:49:47 -0500

    deleted files

--- src/config/config.js
+++
@@ -1,5 +0,0 @@
-const env = {
-  API_KEY: '1c5d1a7c798548e89c61bd4a6db4af54'
-}
-
-export default env
--- src/db/db.js
+++
@@ -1,7 +0,0 @@
-import firebase from './firebase.config';
-
-const db = firebase.firestore();
-
-db.settings({ timestampsInSnapshots: true });
-
-export default db;
